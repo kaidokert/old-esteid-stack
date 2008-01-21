bd53bdb47f4fdc37a1184149904b70c81f1157d5
kaidokert <kaidokert@28238cf3-8f3b-0410-a026-c956796b7a59>
2008-01-21 19:26:58 +0000

    Moved remotely

---
+++ doclean.sh
@@ -0,0 +1,24 @@
+make distclean maintainer-clean distclean-am
+rm config.guess configure config.sub Makefile.in *~ ABOUT-NLS INSTALL Makefile config.log config.status missing aclocal.m4 depcomp install-sh *.spec src/Setup.h.in src/Makefile.in config.rpath
+rm po/POTFILES po/Makefile po/Makefile.in
+rm -rf autom4te.cache
+rm -rf src/.deps
+rm src/Makefile src/Setup.h src/stamp-h1
+rm -rf m4/
+rm -r *~
+rm po/*~
+rm po/*.sed
+rm po/Makevars.template
+rm po/*.gmo
+rm po/*.sin
+rm po/*.header
+rm po/ChangeLog
+rm po/Makefile.in.in
+rm po/Rules-quot
+rm po/stamp-po
+rm src/*~
+rm -rf src/cardlib/html src/cardlib/latex
+rm ./EsteidUtil
+rm -rf ru et src/Release src/Debug
+#rm src/resources/*.xpm 
+rm src/resources/*.cdata 
