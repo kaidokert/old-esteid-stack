b78da8e0e7d10875aadd1e87ad91969d1c58ef14
kaidokert <kaidokert@28238cf3-8f3b-0410-a026-c956796b7a59>
2008-01-21 19:22:57 +0000

    Moved remotely

---
+++ linuxpackage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+./configure --with-wx-config=/home/kaido/Projects/wxWidgets-2.8.5/staticinst/bin/wx-config
+make clean
+make -C po/ update-po
+make -C po/ update-gmo
+make
+
+cd src/cardlib/ && doxygen && zip -r ../../cardlib_basic_docs.zip html && cd ../..
+
+cp src/EsteidUtil .
+rm -rf et ru
+mkdir -p et ru
+cp po/et.po et
+cp po/ru.po ru
+cp po/et.gmo et/EsteidUtil.mo
+cp po/ru.gmo ru/EsteidUtil.mo
+
+tar cvzf EsteidUtil_`grep PACKAGE_VERSION src/Setup.h | sed -e 's/.* \"//' -e 's/\"//'`_Linux86Static.tar.gz \
+	EsteidUtil et/ ru/ ChangeLog cardlib_basic_docs.zip
