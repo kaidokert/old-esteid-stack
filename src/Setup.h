1b08235330fee5943a4325a4d165c30769e5eabf
kaidokert <kaidokert@28238cf3-8f3b-0410-a026-c956796b7a59>
2008-02-19 09:17:39 +0000

    new version

--- src/Setup.h
+++ src/Setup.h
@@ -63,19 +63,19 @@
 #define PACKAGE_NAME "EsteidUtil"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "EsteidUtil 0.9.12"
+#define PACKAGE_STRING "EsteidUtil 0.9.13"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "esteidutil"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.9.12"
+#define PACKAGE_VERSION "0.9.13"
 
 /* rc version */
-#define RC_VERSION 0,9,12,0
+#define RC_VERSION 0,9,13,0
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.9.12"
+#define VERSION "0.9.13"
