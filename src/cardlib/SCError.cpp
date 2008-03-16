9b1d6919aeae6fb31a1a5f997b2dc1c468f69620
kaidokert <kaidokert@28238cf3-8f3b-0410-a026-c956796b7a59>
2008-03-16 18:50:55 +0000

    msg cleanup

--- src/cardlib/SCError.cpp
+++ src/cardlib/SCError.cpp
@@ -14,10 +14,10 @@ SCError::SCError(long err) : error(err),runtime_error("smart card API error")
 	std::ostringstream buf;
 	switch(err) {
 		case 0x80100017 : //SCARD_E_READER_UNAVAILABLE
-			buf << "No smartcard readers available"; break;
+			buf << "No smart card readers"; break;
 		case 0x80100068 : //SCARD_W_RESET_CARD
 		case 0x80100011 : //SCARD_E_INVALID_VALUE .. needs trapping
-			buf << "another application is using the card";break;
+			buf << "Another application is using the card";break;
 		default:
 			buf << "exception:'" << runtime_error::what() << 
 				"' code:'0x" <<std::hex << std::setfill('0') <<
