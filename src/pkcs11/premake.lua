bf75d5656c81a907b6badf53b86f8f294073814c
kaidokert <kaidokert@28238cf3-8f3b-0410-a026-c956796b7a59>
2008-12-04 08:59:14 +0000

    pkcs11 initial implementation added

--- src/pkcs11/premake.lua
+++ src/pkcs11/premake.lua
@@ -1,15 +1,18 @@
 package = newpackage()
-package.name = "cardlib"
+package.name = "esteidpkcs11-1.0"
 package.language = "c++"
-package.kind = "lib"
+package.kind = "dll"
+package.links = { "cardlib"}
 
 package.files = {
   matchfiles("*.h","*.cpp"),
 }
 
-package.buildflags = {"extra-warnings","fatal-warnings"}
+package.includepaths = { "..","pkcs11" }
 
-package.includepaths = {".."}
+package.buildflags = {"extra-warnings","fatal-warnings"}
+package.libpaths = {"../cardlib"}
+
 if (linux) then
 	table.insert(package.includepaths, "/usr/include/PCSC")
 end
