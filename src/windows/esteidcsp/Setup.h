4d049be1a167a557bcb4b871ac1bbc166a54895d
kaidokert <kaidokert@28238cf3-8f3b-0410-a026-c956796b7a59>
2009-08-11 20:25:31 +0000

    version bump

--- src/windows/esteidcsp/Setup.h
+++ src/windows/esteidcsp/Setup.h
@@ -17,10 +17,10 @@
 #define PACKAGE_NAME "EstEID CSP" PKG_BIT PKG_BUILD
 
 /* rc version */
-#define RC_VERSION 2,2,11
+#define RC_VERSION 2,2,13
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.2.11"
+#define PACKAGE_VERSION "2.2.13"
 
 /* Name of package */
 #define PACKAGE "esteidcsp.dll"
